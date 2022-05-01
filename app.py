f51625bb49514c8cdef78c939f177f01569f9424
I'm Blue <kotenkovsn@gmail.com>
2022-05-01 12:42:32 +0700

    Немного изменен код

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import logging
-from path_file import POST_PATH, UPLOAD_FOLDER
-from flask import Flask, request, render_template, send_from_directory
-from functions import is_filename_allowed, load_json, upload_post, search
+from flask import Flask, send_from_directory
 
-from main.main import main_blueprint
-from loader.loader import loader_blueprint
+from main.views import main_blueprint
+from loader.views import loader_blueprint
 
-logging.basicConfig(filename="info.log", level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s', encoding="utf-8")
 
 app = Flask(__name__)
 
@@ -18,11 +15,18 @@ app.register_blueprint(loader_blueprint)
 
 
 @app.errorhandler(413)
-def page_not_found(e):
+def page_not_found():
+    """
+    Обработка ошибки 413
+    """
     logging.error("Загрузка файла больше установленного размера!")
     return "<h1>Файл большеват</h1><p>Поищите поменьше, плиз!</p>", 413
 
 
+@app.route("/uploads/<path:path>")
+def static_dir(path):
+    return send_from_directory("uploads", path)
+
+
 if __name__ == '__main__':
     app.run(Debug=True)
-
